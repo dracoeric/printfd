1b975d40b1a8cc2d43bb3c9e1c828942e94dc865
Eric LI <erli@student.42.fr>
2019-03-04 10:47:05 +0100

    Correction add_width & 3 => & 3 == 3

--- srcs/pfd_add_width.c
+++ srcs/pfd_add_width.c
@@ -6,7 +6,7 @@
 /*   By: erli <erli@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/26 16:52:16 by erli              #+#    #+#             */
-/*   Updated: 2019/03/04 10:45:32 by erli             ###   ########.fr       */
+/*   Updated: 2019/03/04 10:46:53 by erli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,7 @@ static	int	pfd_manage_pound_ox(t_pfd_data *data, char **str, size_t *len)
 
 	if ((size_t)data->tag->width <= *len || data->tag->flags & MINUS)
 		return (pfd_add_str(data, *str, *len));
-	if (data->tag->flags & (7 << 17) && data->tag->flags & POUND
-		&& data->tag->flags & ZERO)
+	if (data->tag->flags & (7 << 17) && (data->tag->flags & 3) == 3)
 	{
 		nb_char = (data->tag->flags & O_CONV ? 1 : 2);
 		if (pfd_add_str(data, *str, nb_char) < 0)
